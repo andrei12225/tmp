b6d9f9e590e001a6bb8cb3ae7dfa2db0e52435c0
andrei <andrei12225@outlook.com>
2025-12-11 13:24:01 +0200

    Rezolvare cu functie

--- bogus_ip.sh
+++ bogus_ip.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cat "/etc/hosts" | while read ip host _; do
-    if [[ "$ip" == "#"* || "$ip" == :* ]]; then 
-        continue 
+bogus() {
+    name="$1"
+    addr="$2"
+    server="$3"
+    real_ip=$(nslookup "$name" "$server"  2>/dev/null | grep -e "Address: .*\..*\..*\..*" | tail -n 1 | awk '{print $2}')
+    if [ ! "$addr" == "$real_ip" ]; then
+        echo "Bogus IP for $name!"
+    else
+	echo "IP-ul este corect"
     fi
+}
 
-    real_ip=$(nslookup "$host" 2>/dev/null | grep -e "Address: .*\..*\..*\..*" | tail -n 1 | awk '{print $2}')
-    if [[ "$ip" == "#"* || "$ip" == :* ]]; then 
-        continue 
-    fi
-    echo "For $host este $real_ip"
-    if [ ! "$ip" == "$real_ip" ]; then
-        echo "Bogus IP for $host in /etc/hosts!"
-    fi
-done
+bogus $1 $2 $3
